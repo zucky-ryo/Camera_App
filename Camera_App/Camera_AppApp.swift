803d643b1ad534139a64bccd43d3bad4eb3a17cd
zucky-ryo <ryota980117@ezweb.ne.jp>
2021-02-12 17:49:26 +0900

    Initial Commit

---
+++ Camera_App/Camera_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_AppApp.swift
+//  Camera_App
+//
+//  Created by 鈴木涼太 on 2021/02/12.
+//
+
+import SwiftUI
+
+@main
+struct Camera_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
